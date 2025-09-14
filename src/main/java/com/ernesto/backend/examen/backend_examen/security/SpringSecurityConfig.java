7428dc203241ccae232ea7a369bf3e221449d1a2
Ernesto Pang <ernestopang513@hotmail.com>
2025-09-13 22:22:51 -0600

    feat: validaciones hardcodeada en springSecurity

--- src/main/java/com/ernesto/backend/examen/backend_examen/security/SpringSecurityConfig.java
+++ src/main/java/com/ernesto/backend/examen/backend_examen/security/SpringSecurityConfig.java
@@ -3,14 +3,17 @@ package com.ernesto.backend.examen.backend_examen.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.ernesto.backend.examen.backend_examen.security.filter.JwtAuthenticationFilter;
+import com.ernesto.backend.examen.backend_examen.security.filter.JwtValidationFilter;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -32,11 +35,13 @@ public class SpringSecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests((authz)-> authz
+        .requestMatchers(HttpMethod.GET, "/api/users").permitAll()
         .anyRequest()
-        .permitAll() )
+        .authenticated() )
         .addFilter(new JwtAuthenticationFilter(authenticationManager()))
-        // .addFilter(new JwtValidator)
+        .addFilter(new JwtValidationFilter(authenticationManager()))
         .csrf(config -> config.disable())
+        .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .build();
     }
 }
